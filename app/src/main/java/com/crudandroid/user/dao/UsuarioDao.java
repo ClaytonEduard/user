5b1ceb88a47692536a500f242427fac336adbe71
ClaytonEduard <clayton_eduard@hotmail.com>
2023-01-15 19:29:11 -0300

    First commit

---
+++ app/src/main/java/com/crudandroid/user/dao/UsuarioDao.java
@@ -0,0 +1,26 @@
+package com.crudandroid.user.dao;
+
+import com.crudandroid.user.entity.Usuario;
+
+import java.util.List;
+
+public class UsuarioDao {
+
+    //metodo salvar
+
+    public void salvar(Usuario usuario){
+
+    }
+
+    public void alterar(Usuario usuario){
+
+    }
+
+    public List<Usuario> listar(){
+        return null;
+    }
+
+    public void excluir(Integer id){
+
+    }
+}
